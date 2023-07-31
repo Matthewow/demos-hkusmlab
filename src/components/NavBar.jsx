f5d38cd7e3e44d54b985927ac6ad5b01e76b6d77
Matthew WANG <matthew.wang@prudential.com.cn>
2023-07-31 17:16:37 +0800

    init structure

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import { AppBar, Box, Typography } from '@mui/material'
+
+export const NavBar = () => {
+  return (
+    <AppBar>
+      <Box>
+        <Typography>NavBar</Typography>
+      </Box>
+    </AppBar>
+  )
+}
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import { Typography } from '@mui/material'
+import { NavBar } from '../components/NavBar'
+
+export const MainPage = () => {
+  return (
+    <Box>
+      <NavBar />
+      <Typography>MainPage</Typography>
+    </Box>
+  )
+}
