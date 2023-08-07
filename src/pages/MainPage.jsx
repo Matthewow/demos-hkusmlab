43c208b30f3eaccd8ed5894cf090aa4538a95af8
Matthew WANG <41960338+Matthewow@users.noreply.github.com>
2023-08-07 22:47:43 +0800

    re-write mainpage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
@@ -12,51 +12,52 @@ import {
   Grid,
   InputLabel,
   MenuItem,
+  Popover,
   Select,
   Stack,
+  TextField,
 } from '@mui/material'
+import { NavBar } from '../components/NavBar'
+import { PopoverIconTitle } from '../components/PopoverIconTitle'
+import Lottie from 'lottie-react'
+import animationData from './animation_order.json'
 
 export function MainPage(props) {
+  const [algotype, setAlgotype] = useState('')
+  const [radius, setRadius] = useState(0)
+
   return (
     <React.Fragment>
-      <AppBar
-        sx={{ backgroundColor: 'rgba(0, 0, 0, 0.3)', visibility: 'hidden' }}
-      >
-        <Toolbar sx={{ height: 90 }} variant="regular">
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-            width={'100%'}
-          >
-            <Stack direction="row" alignItems="center">
-              <Box
-                component="img"
-                src="static/hku-logo.png"
-                sx={{ height: 40, width: 40, pr: 2 }}
-              />
-              <Typography variant="h6">
-                Smart Mobility Lab at HKU | Driver-Order Matching Dashboard
-              </Typography>
-            </Stack>
+      <NavBar />
 
-            <Button sx={{ color: 'white' }}>
-              <Typography>ABOUT US</Typography>
-            </Button>
-          </Stack>
-        </Toolbar>
-      </AppBar>
       <Container sx={{ mt: 20 }} maxWidth="xl">
         <Grid container spacing={2}>
           <Grid item lg={5}>
-            <Card variant="elevation" sx={{ paddingX: 3 }}>
+            <Card variant="elevation" sx={{ paddingX: 3, paddingBottom: 3 }}>
               <Typography variant="h5" sx={{ paddingY: 3 }}>
                 Inputs
               </Typography>
               <Divider variant="fullWidth" />
-              <Typography variant="h5" sx={{ paddingY: 3 }}>
-                Inputs
-              </Typography>
+              <PopoverIconTitle
+                title="Algorithm Type"
+                popoverContent={
+                  <>
+                    <Typography sx={{ p: 1 }} variant="body1">
+                      Broadcasting In the broadcasting phase, the system
+                      announces available orders to drivers. This is typically
+                      done within a certain radius or zone to ensure that only
+                      nearby drivers, who are likely to be able to fulfill the
+                      order in a timely manner, receive the broadcast.
+                    </Typography>
+                    <Divider />
+                    <Typography sx={{ p: 1 }} variant="body1">
+                      Dispatching Dispatching involves assigning the order to a
+                      specific driver. Once the drivers have received the
+                      broadcast, they can choose to accept or reject the order.
+                    </Typography>
+                  </>
+                }
+              />
               <FormControl fullWidth>
                 <InputLabel id="demo-simple-select-helper-label">
                   Order Matching Method
@@ -65,7 +66,8 @@ export function MainPage(props) {
                   labelId="demo-simple-select-label"
                   id="demo-simple-select"
                   label="Order Matching Method"
-                  // onChange={handleChange}
+                  value={algotype}
+                  onChange={(e) => setAlgotype(e.target.value)}
                 >
                   <MenuItem key="dispatch" value={'dispatch'}>
                     <Typography variant="body1">Dispatch</Typography>
@@ -75,12 +77,60 @@ export function MainPage(props) {
                   </MenuItem>
                 </Select>
               </FormControl>
+
+              <Divider variant="fullWidth" />
+
+              <PopoverIconTitle
+                title="Radius"
+                popoverContent={
+                  <Typography sx={{ p: 1 }} variant="body1">
+                    Radius means blabla, with range should be from 1 - 1000
+                  </Typography>
+                }
+              />
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="Number of range 1 - 1000"
+                variant="outlined"
+                onChange={(e) => setRadius(e.target.value)}
+              />
+              <Divider variant="fullWidth" />
+
+              <PopoverIconTitle
+                title="Driver Data Table"
+                popoverContent={
+                  <Typography sx={{ p: 1 }} variant="body1">
+                    Driver Data Table here
+                  </Typography>
+                }
+              />
+
+              <Button variant="outlined">Upload CSV file</Button>
+
+              <PopoverIconTitle
+                title="Order Data Table"
+                popoverContent={
+                  <Typography sx={{ p: 1 }} variant="body1">
+                    Order Data Table here
+                  </Typography>
+                }
+              />
+
+              <Button variant="outlined">Upload CSV file</Button>
             </Card>
           </Grid>
 
           <Grid item lg={7}>
             <Card variant="elevation" sx={{ p: 3 }}>
-              dfa
+              <Typography variant="h5" sx={{ paddingY: 3 }}>
+                Results
+              </Typography>
+              <Container>
+                <Box maxWidth="50%">
+                  <Lottie loop={false} animationData={animationData} />
+                </Box>
+              </Container>
             </Card>
           </Grid>
         </Grid>
