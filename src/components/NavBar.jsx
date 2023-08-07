caf8f3cbd9bb9d04ee4ab4882889c68cc331a6cb
Matthew WANG <41960338+Matthewow@users.noreply.github.com>
2023-08-07 22:47:06 +0800

    added NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,31 @@
-import { AppBar, Box, Typography } from '@mui/material'
+import { AppBar, Box, Button, Stack, Toolbar, Typography } from '@mui/material'
 
 export const NavBar = () => {
   return (
-    <AppBar>
-      <Box>
-        <Typography>NavBar</Typography>
-      </Box>
+    <AppBar sx={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}>
+      <Toolbar sx={{ height: 90 }} variant="regular">
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          width={'100%'}
+        >
+          <Stack direction="row" alignItems="center">
+            <Box
+              component="img"
+              src="static/hku-logo.png"
+              sx={{ height: 40, width: 40, pr: 2 }}
+            />
+            <Typography variant="h6">
+              Smart Mobility Lab at HKU | Driver-Order Matching Dashboard
+            </Typography>
+          </Stack>
+
+          <Button sx={{ color: 'white' }}>
+            <Typography>ABOUT US</Typography>
+          </Button>
+        </Stack>
+      </Toolbar>
     </AppBar>
   )
 }
