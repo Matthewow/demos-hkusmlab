a406daab368cdb8b3fdf9dceb92e6e49521a6cc1
Matthew WANG <41960338+Matthewow@users.noreply.github.com>
2023-08-10 01:10:55 +0800

    color changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { MainPage } from './pages/MainPage'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import { Box } from '@mui/material'
 
 function App() {
+  useEffect(() => {
+    document.body.style.backgroundColor = '#e3e3e3' // Change to the color you want
+  }, [])
   return <MainPage />
 }
 
