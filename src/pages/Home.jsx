8caf681851298061b61116c42d6b164179dec6d6
Matthew WANG <matthewwang.study@gmail.com>
2023-10-01 23:15:38 +0800

    init home page basic layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,57 @@
 import { Box, Typography } from '@mui/material'
 import React from 'react'
+import backgroundImage from '../images/home-background-image.jpg'
+
 export const HomePage = () => {
+  const backgroundTransparency = 0.6
+
+  const rootStyles = {
+    position: 'relative',
+    '&::before': {
+      content: '""',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, ${backgroundTransparency}), rgba(0, 0, 0, ${backgroundTransparency})), url(${backgroundImage})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      zIndex: -1,
+    },
+  }
+
+  const contentStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    color: 'white',
+    fontWeight: 800,
+  }
+
   return (
-    <Box
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-      }}
-    >
-      <Typography variant="h1">Demos Home Page</Typography>
-      <Typography variant="subtitle1">To be done</Typography>
-    </Box>
+    <>
+      <Box sx={rootStyles}>
+        <Box sx={contentStyles}>
+          <Typography variant="h1" fontWeight={800}>
+            HKU Smart Mobility Lab
+          </Typography>
+          <Typography variant="h1" fontWeight={800}>
+            Research DEMOs
+          </Typography>
+          <Typography variant="body1">
+            Scroll down for our available demos ▼
+          </Typography>
+        </Box>
+      </Box>
+      <Box sx={{ m: 30 }}>
+        <Typography variant="h4" fontWeight={500}>
+          In development...
+        </Typography>
+      </Box>
+    </>
   )
 }
