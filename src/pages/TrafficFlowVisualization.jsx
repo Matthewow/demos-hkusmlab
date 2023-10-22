75e6026934f44a0293e94927f0375220f7e1e47d
Matthew WANG <matthewwang.study@gmail.com>
2023-10-22 19:19:03 +0800

    added one more package and text changed

--- src/pages/TrafficFlowVisualization.jsx
+++ src/pages/TrafficFlowVisualization.jsx
@@ -7,21 +7,15 @@ const title = `Visualization of Urban Road Traffic Flow in Hong Kong`
 const about = () => {
   return (
     <Typography variant="subtitle1" align="left" gutterBottom>
-      This demo presents a novel approach for visualizing real-time urban road
-      traffic flow in Hong Kong. By harnessing the power of advanced geospatial
-      technologies and traffic sensor data, we have developed an interactive map
-      interface that provides comprehensive insights into the city's dynamic
-      traffic patterns. Users can view the overall traffic situation at a glance
-      or delve into more granular details by simply clicking on a specific road
-      on the interactive map. The clicked road segment reveals current traffic
-      flow data, including the number of vehicles traversing that route in
-      real-time. This system not only aids in understanding the current state of
-      traffic but also provides valuable data for urban planning, traffic
-      management, and policy-making. With its user-friendly design and real-time
-      data access, this visualization tool serves as a valuable resource for
-      city planners, traffic engineers, and the general public alike,
-      contributing to more informed decisions and smarter, more sustainable
-      urban development.
+      This demo showcases an interactive map interface for real-time traffic
+      flow visualization in Hong Kong. Utilizing advanced geospatial
+      technologies and sensor data, it provides detailed insights into city-wide
+      traffic patterns. Users can view the overall traffic scenario or access
+      granular data for specific roads, including real-time vehicle counts. This
+      tool is valuable for urban planning, traffic management, and
+      policy-making, aiding in data-driven decisions for sustainable city
+      development. It serves city planners, traffic engineers, and the public
+      with its accessible design and real-time data.
     </Typography>
   )
 }
