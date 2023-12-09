45260634ce6985dee438216966d2fc17c56c6ad6
Matthew Wang <matthewwang.study@gmail.com>
2023-12-09 17:06:21 +0800

    finished navigation web app

--- src/pages/navigation-ride-hailing/components/Map.jsx
+++ src/pages/navigation-ride-hailing/components/Map.jsx
@@ -1,54 +1,117 @@
 import { Box, Container } from '@mui/material'
-import mapboxgl from '!mapbox-gl' // eslint-disable-line import/no-webpack-loader-syntax
+import mapboxgl from 'mapbox-gl'
 import { useEffect, useRef, useState } from 'react'
 import { appConfigs } from '../../../appConfigs'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import { navigationPost } from '../../../utils/http'
 
 mapboxgl.accessToken = appConfigs.mapboxAccessToken
 
-export const MapContainer = (props) => {
-  const { lng_init, lat_init } = props
-  const mapContainer = useRef(null)
-  const map = useRef(null)
-  const marker = useRef(null)
-  const [lng, setLng] = useState(lng_init)
-  const [lat, setLat] = useState(lat_init)
-  const [zoom, setZoom] = useState(15)
+const MAP_STYLE = 'mapbox://styles/mapbox/dark-v11'
+const ZOOM_LEVEL = 15
+
+export const MapContainer = ({ lng_init, lat_init }) => {
+  const mapContainerRef = useRef(null)
+  const mapRef = useRef(null)
+  const markerRef = useRef(null)
+
+  const [coords, setCoords] = useState({
+    lng: lng_init,
+    lat: lat_init,
+    zoom: ZOOM_LEVEL,
+  })
 
   useEffect(() => {
-    if (!map.current) {
-      map.current = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: 'mapbox://styles/mapbox/dark-v11',
+    const initializeMap = ({ lng, lat, zoom }) => {
+      if (mapRef.current) return // Do not initialize the map if it already exists
+
+      mapRef.current = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        style: MAP_STYLE,
         center: [lng, lat],
         zoom: zoom,
         attributionControl: false,
         logoPosition: 'top-left',
       })
 
-      map.current.on('move', () => {
-        setLng(map.current.getCenter().lng.toFixed(4))
-        setLat(map.current.getCenter().lat.toFixed(4))
-        setZoom(map.current.getZoom().toFixed(2))
+      mapRef.current.on('move', () => {
+        const { lng, lat } = mapRef.current.getCenter()
+        const zoom = mapRef.current.getZoom()
+        setCoords({
+          lng: parseFloat(lng.toFixed(4)),
+          lat: parseFloat(lat.toFixed(4)),
+          zoom: parseFloat(zoom.toFixed(2)),
+        })
       })
 
-      map.current.on('load', () => {
-        if (marker.current) marker.current.setLngLat([lng_init, lat_init])
-        else {
-          marker.current = new mapboxgl.Marker()
-            .setLngLat([lng_init, lat_init])
-            .addTo(map.current)
+      markerRef.current = new mapboxgl.Marker()
+        .setLngLat([lng, lat])
+        .addTo(mapRef.current)
+    }
+
+    initializeMap(coords)
+
+    return () => {
+      if (mapRef.current) {
+        mapRef.current.remove()
+        mapRef.current = null
+      }
+    }
+  }, [])
+  useEffect(() => {
+    if (mapRef.current && markerRef.current) {
+      // Update map center and marker position
+      mapRef.current.setCenter([lng_init, lat_init])
+      markerRef.current.setLngLat([lng_init, lat_init])
+
+      // Get the new route and draw it
+      navigationPost(`${lat_init}_${lng_init}`).then((newRoute) => {
+        // First, check if there's already a source for the route
+        if (mapRef.current.getSource('route')) {
+          // Update the route data
+          mapRef.current.getSource('route').setData({
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: newRoute,
+            },
+          })
+        } else {
+          // Add a new source and layer for the route
+          mapRef.current.addSource('route', {
+            type: 'geojson',
+            data: {
+              type: 'Feature',
+              properties: {},
+              geometry: {
+                type: 'LineString',
+                coordinates: newRoute,
+              },
+            },
+          })
+
+          mapRef.current.addLayer({
+            id: 'route',
+            type: 'line',
+            source: 'route',
+            layout: {
+              'line-join': 'round',
+              'line-cap': 'round',
+            },
+            paint: {
+              'line-color': '#fcba03', // Choose a color for the route line
+              'line-width': 6,
+            },
+          })
         }
       })
-    } else {
-      map.current.setCenter([lng_init, lat_init])
-      if (marker.current) marker.current.setLngLat([lng_init, lat_init])
     }
   }, [lng_init, lat_init])
 
   return (
-    <Container style={{ width: '100%', height: '100%' }}>
-      <Box style={{ width: '100%', height: 600 }} ref={mapContainer} />
+    <Container sx={{ width: '100%', height: '100%' }}>
+      <Box sx={{ width: '100%', height: 600 }} ref={mapContainerRef} />
     </Container>
   )
 }
